7d4e1501b1a048b1344cd185bbf8ca92c0a4f35b
a1mecarte <aga.i.mecarte@gmail.com>
2023-03-03 14:37:22 +0800

    activity 1: create a script to find the average age of all adults in an aray

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,25 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+def adults_average(ages)
+
+  adults_total = 0
+  adults_counter = 0
+
+  ages.each do |age|
+
+    if age >= 18
+      adults_total = adults_total + age
+      adults_counter = adults_counter + 1
+    end
+
+  end
+
+  average = adults_total / adults_counter
+
+  return average
+
+end
+
+p adults_average([19, 24, 10, 15, 36])
