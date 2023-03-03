595b8a5dd06a9335f098dd3ebabdf2329a55b016
a1mecarte <aga.i.mecarte@gmail.com>
2023-03-03 15:34:56 +0800

    activity 4: create a script that returns an array of unique numbers for a reference array

--- activities/activity_4.rb
+++ activities/activity_4.rb
@@ -2,3 +2,30 @@
 #  Write a function that takes an array of ages.
 #  The function will return an Array of ages that you could only see one time.
 #  Print on console the result.
+
+def one_time_ages(ages)
+
+  new_array = []
+  reference_array = ages
+
+  ages.each do |age|
+
+    counter = 0
+
+    reference_array.each do |value|
+      if value == age
+        counter = counter + 1
+      end
+    end
+
+    if counter == 1
+      new_array << age
+    end
+
+  end
+
+  return new_array
+
+end
+
+p one_time_ages([1,1,2,3,3,4,5])
