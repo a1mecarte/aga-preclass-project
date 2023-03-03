1f9950f298d8e5b4119c674d4a222ed611b6ff60
a1mecarte <aga.i.mecarte@gmail.com>
2023-03-03 14:54:38 +0800

    activity 2: create a script that combines all name values into one full name in a hash

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,19 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+name = {first_name: "Aga", middle_name: "Iyog", last_name: "Mecarte"}
+
+def print_full_name(name)
+
+  fullname = ""
+
+  name.each do |key, value|
+    fullname = fullname + value + " "
+  end
+
+  p fullname
+
+end
+
+print_full_name(name)
